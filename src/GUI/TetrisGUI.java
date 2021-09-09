7ec450e084a03ad886955a960348f5167ee84f0c
Ezequiel74452 <ezequielcaviglione@hotmail.com>
2021-09-09 15:53:24 -0300

    Commit 2
    Actualización de ícono, dimensiones de la ventana y layout.

--- src/GUI/TetrisGUI.java
+++ src/GUI/TetrisGUI.java
@@ -7,6 +7,14 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import gui.SimplePresentationScreen;
+
+import java.awt.Toolkit;
+import java.awt.Frame;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+
 public class TetrisGUI extends JFrame {
 
 	private JPanel contentPane;
@@ -31,13 +39,17 @@ public class TetrisGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public TetrisGUI() {
+		setResizable(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(TetrisGUI.class.getResource("/images/icon.png")));
 		setTitle("Tetris");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 600, 490);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new FormLayout(new ColumnSpec[] {},
+			new RowSpec[] {}));
+		// Fullscreen: setExtendedState(6);
 	}
 
 }
